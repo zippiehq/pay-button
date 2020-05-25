e292c6be9d9dc35670e4e6c07c494632ca3caa89
Patrik Åkerlund <akerlund.patrik@gmail.com>
2020-05-25 21:47:45 +0200

    Update zippie.js

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,16 @@
-// Demo example of Zippie Pay
+// Demo example of Zippie Pay API
 
 document.getElementById('buyButton').addEventListener('click', onBuyClicked)
 
 function onBuyClicked() {
+  // Get user input data
   const paymentData = {
-    merchantId: 'demo',
+    // MerchantId must be an Ethereum Address (public key) you own (you posses the private key)
+    // it can also be a Zippie ENS name for this address
+    merchantId: 'demo.zippie',
+    // OrderId must be a 256-bit value
+    orderId:
+      '0x0000000000000000000000000000000000000000000000000000000000000001',
     amount: document.getElementById('amount').value,
     currency: 'KSH',
     email: document.getElementById('email').value,
@@ -16,14 +22,17 @@ function onBuyClicked() {
   }
 
   try {
+    // Call Zippie Pay API (Chrome/Android will open popup using Payment Request API, Safari/iOS will open a new tab in fullscreen)
     const request = zippie.paymentRequest(paymentData)
     request
       .show()
       .then(function (result) {
+        // Payment succesful
         console.log('zippie.paymentRequest result', result)
         info(result)
       })
       .catch(function (error) {
+        // Payment failed (e.g. cancel)
         console.error('zippie.paymentRequest error', error)
         info(error)
       })
--- public/zippie.js
+++ public/zippie.js
@@ -13,24 +13,23 @@ const zippie = (function () {
         const supportedInstruments = [
           {
             supportedMethods: 'https://dev.zippie.com',
-            //supportedMethods: 'https://klaatu-zippie.ngrok.io',
             data: {
-              merchantIdentifier: this.paymentData.merchantId,
+              merchantId: this.paymentData.merchantId,
+              orderId: this.paymentData.orderId,
               email: this.paymentData.email,
             },
           },
         ]
 
         const details = {
-          // XXX: pass email and merchanId here since method data is currently not coming thought to sw
-          id: `${this.paymentData.email}:${this.paymentData.merchantId}`,
+          id: this.paymentData.orderId,
           total: {
             label: 'Total',
             amount: {
               currency: this.paymentData.currency,
               value: this.paymentData.amount,
             },
-          },
+          },    
         }
 
         this.request = new PaymentRequest(supportedInstruments, details)
@@ -41,20 +40,14 @@ const zippie = (function () {
         }
 
         return new Promise(async (resolve, reject) => {
-          this.request
-            .show()
-            .then(function (response) {
-              console.info(response)
-              response.complete('success')
-              resolve('ok')
-              // XXX: What should we return?
-              //resolve(response)
-            })
-            .catch(function (error) {
-              console.info(error)
-              reject('error')
-              //reject(error)
-            })
+          this.request.show().then(function (response) {
+            console.info(response)
+            response.complete('success')
+            resolve(response)
+          }).catch(function (error) {
+            console.info(error)
+            reject(error)
+          })
         })
       } catch (e) {
         console.error(e.message)
@@ -65,22 +58,18 @@ const zippie = (function () {
   function openInFullscreen(paymentData) {
     return new Promise(async (resolve, reject) => {
       // XXX: Focus instead of new window if we have one already?
-      // XXX: Redo parmas?
       window.open(
-        //`https://klaatu-zippie.ngrok.io/#pay=${paymentData.amount}:${paymentData.currency}:${paymentData.email}:${paymentData.merchantId}`,
-        `https://dev.zippie.com/#pay=${paymentData.amount}:${paymentData.currency}:${paymentData.email}:${paymentData.merchantId}`
+        `https://dev.zippie.com/#pay-fullscreen=${paymentData.merchantId}/${paymentData.orderId}/${paymentData.amount}/${paymentData.currency}/${paymentData.email}`,
       )
-      // XXX: Send this from Klaatu, Check if window has been closed
+      // Wait for response
+      // XXX: Also check if window has been closed manually?
       window.onmessage = function (e) {
-        if (e.data === 'zippie pay success') {
-          resolve('ok')
+        if (e.data.status === 'ok') {
+          resolve(e.data)
+          return
         }
-        if (e.data === 'zippie pay error') {
-          reject('error')
-        }
+        reject(e.data)
       }
-
-      resolve('ok')
     })
   }
 
