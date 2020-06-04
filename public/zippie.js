446ac0e7d5e61c6e815a53b2539b04bc3342d845
Patrik Åkerlund <akerlund.patrik@gmail.com>
2020-06-04 08:38:14 +0200

    Use dev.z.c for public api (latest + encoded data)

--- public/zippie.js
+++ public/zippie.js
@@ -2,41 +2,22 @@ const zippie = (function () {
   class ZippiePaymentRequest {
     constructor(paymentData) {
       this.paymentData = paymentData
+      this.paymentDataEncoded = window.btoa(JSON.stringify(paymentData))
     }
 
     async show() {
       try {
+
         if (!window.PaymentRequest) {
-          return openInFullscreen(this.paymentData)
+          return openInFullscreen(this.paymentData, this.paymentDataEncoded)
         }
-
-        const supportedInstruments = [
-          {
-            supportedMethods: 'https://testing.zippie.com',
-            data: {
-              merchantId: this.paymentData.merchantId,
-              orderId: this.paymentData.orderId,
-              email: this.paymentData.email,
-            },
-          },
-        ]
-
-        const details = {
-          id: this.paymentData.orderId,
-          total: {
-            label: 'Total',
-            amount: {
-              currency: this.paymentData.currency,
-              value: this.paymentData.amount,
-            },
-          },    
-        }
-
+        
+        const { supportedInstruments, details } = getPaymentRequest(this.paymentData, this.paymentDataEncoded)
         this.request = new PaymentRequest(supportedInstruments, details)
 
         let canMakePayment = await this.request.canMakePayment()
-        if (!canMakePayment) {
-          return openInFullscreen(this.paymentData)
+        if (!canMakePayment || this.paymentData.force !== "false") {
+          return openInFullscreen(this.paymentData, this.paymentDataEncoded )
         }
 
         return new Promise(async (resolve, reject) => {
@@ -55,11 +36,43 @@ const zippie = (function () {
     }
   }
 
-  function openInFullscreen(paymentData) {
+  function getUrl(env) {
+    if (env === 'localhost') {
+      return 'https://localhost:8443'
+    }
+    return `https://${env}.zippie.com`
+  }
+
+  function getPaymentRequest(paymentData, paymentDataEncoded) {
+    const supportedInstruments = [
+      {
+        supportedMethods: getUrl(paymentData.env),
+        data: {
+          paymentData: paymentData, // XXX: Remove?
+          paymentDataEncoded: paymentDataEncoded
+        },
+      },
+    ]
+
+    const details = {
+      id: paymentData.orderId,
+      total: {
+        label: 'Total',
+        amount: {
+          currency: paymentData.currency,
+          value: paymentData.amount,
+        },
+      },
+    }
+
+    return { supportedInstruments, details }
+  }
+
+  function openInFullscreen(paymentData, paymentDataEncoded) {
     return new Promise(async (resolve, reject) => {
       // XXX: Focus instead of new window if we have one already?
       window.open(
-        `https://testing.zippie.com/#pay-fullscreen=${paymentData.merchantId}/${paymentData.orderId}/${paymentData.amount}/${paymentData.currency}/${paymentData.email}`,
+        `${getUrl(paymentData.env)}/#pay-fullscreen=${paymentDataEncoded}`,
       )
       // Wait for response
       // XXX: Also check if window has been closed manually?
@@ -75,17 +88,21 @@ const zippie = (function () {
 
   return {
     paymentRequest: function (paymentData) {
+      // XXX: Validate data
       // const paymentData = {
-      //   merchantId: 'demo'
-      //   country: 'Kenya',
-      //   currency: 'KES',
-      //   total: {
-      //     label: 'Demo total',
-      //     amount: 19,
-      //   }
+      //   merchantId: 'demo.zippie',
+      //   orderId: '000001',
+      //   amount: 1.23 (two decimals => send as 123?)
+      //   currency: 'EUR',
+      //   email: my@email.com, (prefill email field in UI)
+      //   paymentMethods: ['mpesa', 'zippie', 'creditcard'],
+      //   orderDetails: [
+      //    { label: 'T-shirt', value: '100 KSH' },
+      //    { label: 'Fee', value: '5 KSH' },
+      //   ],
+      //   options: { requestPayerName: true, requestPayerEmail: true, forceFullscreenMode: false }, (move to merchant setting)
       // }
-      // XXX: Add options: requestPayerName, requestPayerEmail, forceFullscreenMode, showFee, ... etc?
-      const request = new ZippiePaymentRequest(paymentData)
+      const request = new ZippiePaymentRequest({ env: "dev", force: "false", ...paymentData })
       return request
     },
   }
